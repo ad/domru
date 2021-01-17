cc4c1acf1bd6ccea5826ae11ba2393111dbf2f3f
ad <daniel.apatin@gmail.com>
2021-01-17 18:29:50 +0300

    draw error message on snapshot

--- go.mod
+++ go.mod
@@ -1,3 +1,9 @@
 module github.com/ad/domru
 
-go 1.14
+go 1.15
+
+require (
+	github.com/fogleman/gg v1.3.0
+	github.com/golang/freetype v0.0.0-20170609003504-e2365dfdc4a0 // indirect
+	golang.org/x/image v0.0.0-20201208152932-35266b937fa6 // indirect
+)
--- handlers/snapshot.go
+++ handlers/snapshot.go
@@ -1,11 +1,15 @@
 package handlers
 
 import (
+	"bytes"
 	"fmt"
 	"io/ioutil"
 	"log"
 	"net/http"
 	"strconv"
+    jpeg "image/jpeg"
+
+    "github.com/fogleman/gg"
 )
 
 // SnapshotHandler ...
@@ -42,8 +46,53 @@ func (h *Handler) SnapshotHandler(w http.ResponseWriter, r *http.Request) {
 	defer resp.Body.Close()
 	body, err = ioutil.ReadAll(resp.Body)
 
-	if resp.StatusCode == 200 {
-		w.Header().Set("Content-Type", "image/jpeg")
+	if err == nil {
+		contentType := http.DetectContentType(body)
+
+		if resp.StatusCode != 200 {
+			err = fmt.Errorf("wrong response, code: %d, result: %s", resp.StatusCode, string(body))
+		} else if contentType != "image/jpeg" {
+			err = fmt.Errorf("wrong response, code: %d, Content-Type: %s", resp.StatusCode, contentType)
+		}
+	}
+
+	w.Header().Set("Content-Type", "image/jpeg")
+
+	if resp.StatusCode != 200  {
+		width := 500
+		height := 281
+
+	    dc := gg.NewContext(width, height)
+	    dc.SetHexColor("#000000")
+	    dc.DrawRectangle(0, 0, float64(width), float64(height))
+	    dc.Fill()
+	    dc.SetHexColor("#ffffff")
+	    dc.DrawStringWrapped(
+	    	err.Error(), // text
+	    	float64(width/2), // block x pos
+	    	float64(height/2),// block y pos
+	    	0.5, 			     // text x pos
+	    	0, 					 // text y pos
+	    	float64(width/2), // width
+	    	1, 					 // linespacing
+	    	gg.AlignCenter, 	 // align
+	    )
+	    dc.Clip()
+	    dc.SavePNG("out.png")
+
+	    b := bytes.Buffer{}
+	    var opt jpeg.Options
+		opt.Quality = 75
+		err = jpeg.Encode(&b, dc.Image(), &opt)
+		if err != nil {
+			log.Printf("png.Encode: %s", err)
+			w.WriteHeader(http.StatusInternalServerError)
+			return
+		}
+
+		w.Write(b.Bytes())
+
+		return
 	}
 
 	w.Header().Set("Content-Length", strconv.Itoa(len(body)))
